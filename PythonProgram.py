712d407dbc74bb6c35f7cfa1311e02a7ec51e1ac
wellsdr <wellsdr@miamioh.edu>
2016-10-18 16:56:49 -0400

    Program is complete but for UI and voter verification

--- PythonProgram.py
+++ PythonProgram.py
@@ -6,13 +6,12 @@ Copyright 2016
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-
-class VoteCalc:
+class State:
 
     def __init__(self):
         Tk().withdraw()
         self.voterdatafile = askopenfilename()
-        self.voterverifyfile = askopenfilename()
+        #self.voterverifyfile = askopenfilename()
         self.currentCounty = ""
         self.newCounty = County("")
         self.counties = []
@@ -21,46 +20,29 @@ class VoteCalc:
         #todo implement verification
         return True
 
-    def calcVotes(self):
+    def getCountiesAndDistricts(self):
+        """
+        1. Takes the voter data from the file specified above and reads the info
+        2. Creates counties and saves their districts
+        """
         f = open(self.voterdatafile,'r')
+        
         for line in f:
-            data = [x.strip() for x in line.split(',')]
-            if self.currentCounty != data[0]:
-                self.currentCounty = data[0]
-                self.newCounty = County(data[0])
-                self.counties.append(self.newCounty)
-            newDistrict = District(data[1], data[2], data[3], data[4])
-            self.newCounty.addDistrict(newDistrict)  
+            data = [x.strip() for x in line.split(',')] #separate by comma
+            if self.currentCounty != data[0]: #check if found new county
+                self.currentCounty = data[0] #name county
+                self.newCounty = County(data[0]) #save county
+                self.counties.append(self.newCounty) #add county to lsit
+            newDistrict = District(data[1], data[2], data[3], data[4]) #create district
+            self.newCounty.addDistrict(newDistrict)  #add district to county
 
-class District:
+        for i in range(0,len(self.counties)): #initializes all the counties
+            self.counties[i].getData()
 
-    def __init__(self, name, repubVotes, demVotes, indVotes):
-        self.name = name
-        self.repub = int(repubVotes)
-        self.dem = int(demVotes)
-        self.ind = int(indVotes)
-        self.totalVotes = self.repub + self.dem + self.ind
-
-    def getDemVotes(self):
-        return self.dem
-
-    def getRepubVotes(self):
-        return self.repub
-
-    def getIndVotes(self):
-        return self.ind
-
-    def getTotal(self):
-        return self.totalVotes()
-
-    def getDemPercent(self):
-        return self.dem/self.totalVotes
-
-    def getRepPercent(self):
-        return self.repub/self.totalVotes
-
-    def getIndPercent(self):
-        return self.ind/self.totalVotes
+    def selectCounty(self, name):
+        for i in range(0, len(self.counties)):
+            if name == self.counties[i].getName():
+                return self.counties[i]
 
 class County:
 
@@ -72,29 +54,157 @@ class County:
         self.ind = 0
         self.total = 0
 
+    def selectDistrict(self, name):
+        for i in range(0, len(self.districts)):
+            if name == self.districts[i].getName():
+                return self.districts[i]
+
+    def getName(self):
+        return self.name
+
     def getData(self):
+        """
+        Run this when all the districts have been added to a county
+        1. calculates the total number of votes in a county
+        """
         self.calcDemVotes()
         self.calcRepubVotes()
         self.calcIndVotes()
         self.calcTotalVotes()
 
     def addDistrict(self, district):
+        """
+        @param
+                district, a district object: adds a district to a county
+        """
         self.districts.append(district)
 
     def calcDemVotes(self):
+        """
+        calculates the total number of democratic votes in a county
+        """
         for i in range (0,len(self.districts)):
             self.dem = self.dem + self.districts[i].getDemVotes()
 
+    def getDemVotes(self):
+        """
+        @return
+                int, the number of democratic votes in a county
+        """
+        return self.dem
+
     def calcRepubVotes(self):
+        """
+        calculates the total number of republican votes in a county
+        """
         for i in range (0,len(self.districts)):
             self.repub = self.repub + self.districts[i].getRepubVotes()
 
+    def getRepubVotes(self):
+        """
+        @return
+                int, the number of republican votes in a county
+        """
+        return self.repub
+
     def calcIndVotes(self):
+        """
+        calculates the total number of independent votes in a county
+        """
         for i in range (0,len(self.districts)):
             self.ind = self.ind + self.districts[i].getIndVotes()
 
+    def getIndVotes(self):
+        """
+        @return
+                int, the number of independent votes in a county
+        """
+        return self.ind
+
     def calcTotalVotes(self):
+        """
+        calculates the total number of votes in a county
+        """
         self.total = self.dem + self.repub + self.ind
 
-    def demPercent(self):
-        return 
+    def getDemPercent(self):
+        """
+        @return
+                int, the percentage of democratic votes in a county
+        """
+        return self.dem/self.total
+
+    def getRepubPercent(self):
+        """
+        @return
+                int, the percentage of republican votes in a county
+        """
+        return self.repub/self.total
+
+    def getIndPercent(self):
+        """
+        @return
+                the percentage of independent votes in a county
+        """
+        return self.ind/self.total
+
+class District:
+
+    def __init__(self, name, repubVotes, demVotes, indVotes):
+        self.name = name
+        self.repub = int(repubVotes)
+        self.dem = int(demVotes)
+        self.ind = int(indVotes)
+        self.totalVotes = self.repub + self.dem + self.ind
+
+    def getName(self):
+        return self.name
+
+    def getDemVotes(self):
+        """
+        @return
+                int, the number of democratic votes in a district
+        """
+        return self.dem
+
+    def getRepubVotes(self):
+        """
+        @return
+                int, the number of republican votes in a district
+        """
+        return self.repub
+
+    def getIndVotes(self):
+        """
+        @return
+                int, the number of independent votes in a district
+        """
+        return self.ind
+
+    def getTotal(self):
+        """
+        @return
+                int, the total number of votes in a district
+        """
+        return self.totalVotes()
+
+    def getDemPercent(self):
+        """
+        @return
+                the percentage of democratic voters in a district
+        """
+        return self.dem/self.totalVotes
+
+    def getRepPercent(self):
+        """
+        @return
+                the percentage of republican voters in a district
+        """
+        return self.repub/self.totalVotes
+
+    def getIndPercent(self):
+        """
+        @return
+                the percentage of independent voters in a district
+        """
+        return self.ind/self.totalVotes
