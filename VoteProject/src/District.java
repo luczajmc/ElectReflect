03f4fc6a8eaa16233a486a447397db4dcdaf6cb3
Reese Wells <Reese Wells>
2016-10-24 15:30:09 -0400

    Commented everything

--- VoteProject/src/District.java
+++ VoteProject/src/District.java
@@ -4,27 +4,73 @@ public class District {
 	private int repVotes;
 	private int demVotes;
 	private int indVotes;
-	
-	public District(String name, int data, int data2, int data3) {
+
+	/**
+	 * @param name
+	 * @param republican
+	 * @param democrat
+	 * @param independent
+	 */
+	public District(String name, int republican, int democrat, int independent) {
 		this.name = name;
-		this.repVotes = data;
-		this.demVotes = data2;
-		this.indVotes = data3;
+		this.repVotes = republican;
+		this.demVotes = democrat;
+		this.indVotes = independent;
 	}
 	
+	/**
+	 * @return the name of the District
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * @return the number of democratic votes
+	 */
 	public int getDemVotes() {
 		return this.demVotes;
 	}
 	
+	/**
+	 * @return the number of republican votes
+	 */
 	public int getRepVotes() {
 		return this.repVotes;
 	}
 	
+	/**
+	 * @return the number of independent votes
+	 */
 	public int getIndVotes() {
 		return this.getIndVotes();
+	}
+	
+	/**
+	 * @return the total number of votes
+	 */
+	public int getTotalVotes() { 
+		return this.repVotes + this.demVotes + this.indVotes;
+	}
+	
+	/**
+	 * @return the percentage of democratic votes
+	 */
+	public double getDemPercent() {
+		return this.demVotes / this.getTotalVotes();
+	}
+	
+	/**
+	 * @return the percentage of republican votes
+	 */
+	public double getRepPercent() {
+		return this.repVotes / this.getTotalVotes();
+	}
+	
+	/**
+	 * @return the percentage of independent votes
+	 */
+	public double getIndPercent() {
+		return this.indVotes / this.getTotalVotes();
 	}
 }
