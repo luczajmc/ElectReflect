8d8b46039ef9279f19943768e6620a3641d4c753
Reese Wells <Reese Wells>
2016-10-24 14:08:08 -0400

    Filled out initial methods for State, County, and District

--- VoteProject/src/District.java
+++ VoteProject/src/District.java
@@ -1,4 +1,14 @@
 
 public class District {
-
+	private String name;
+	private int repVotes;
+	private int demVotes;
+	private int indVotes;
+	
+	public District(String name, int data, int data2, int data3) {
+		this.name = name;
+		this.repVotes = data;
+		this.demVotes = data2;
+		this.indVotes = data3;
+	}
 }
