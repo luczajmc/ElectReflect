e89517423451d3c55c6a45a4018318d1291488f1
Steven Bower <Steven Bower>
2016-10-24 15:42:38 -0400

    Got the button position and size controllable

--- VoteProject/src/Gui.java
+++ VoteProject/src/Gui.java
@@ -1,18 +1,31 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Gui extends JPanel{
 	
 	private JFrame window = new JFrame("ElectReflect");
-	private JButton start = new JButton("Start");
+	private JButton start = new JButton();
 	
 	public Gui(){
+		super();
 		window.setBounds(0, 0, 600, 600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(true);
+		this.setLayout(null);
 		window.add(this);
 		window.setVisible(true);
-		window.add(start);
+		
+		start.setText("Start");
+		start.setLayout(null);
+		start.setLocation(0,100);
+		start.setSize(100,50);
+		add(start);
+		start.setMnemonic(KeyEvent.VK_ENTER);
+	}
+	
+	public void start(){
+		
 	}
 	
 	public static void main(String[] args){
