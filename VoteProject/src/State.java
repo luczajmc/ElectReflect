0c46b5ef47500b7e4f1c049d7d53b1177bb441a6
Reese Wells <Reese Wells>
2016-10-24 13:34:04 -0400

    Added Tester method and started State method

--- VoteProject/src/State.java
+++ VoteProject/src/State.java
@@ -1,4 +1,40 @@
+import java.awt.Component;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class State {
-
+	private File voterData;
+	private File verifyData;
+	private String currentCounty;
+	private County newCounty;
+	private ArrayList counties;
+	
+	public State(File voterFile, File verifyFile) {
+		this.voterData = voterFile;
+		this.verifyData = verifyFile;
+	}
+	
+	public static File getFile() {
+		
+		JFrame fileFrame = new JFrame("Choose CSV File");
+		fileFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new File(System.getProperty("user.home")));
+		int result = fc.showOpenDialog(fileFrame);
+		
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File file = fc.getSelectedFile();
+			return file;
+		}
+		return null;
+	}
+	
+	public String toString() {
+		return this.voterData.toString();
+	}
 }
