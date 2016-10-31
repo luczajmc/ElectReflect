7401d390c199d875ae4a35f863295b24b856036c
Matthew Luczaj <luczajmc@miamioh.edu>
2016-10-31 12:51:24 -0400

    Stubbed out graphing functions

--- VoteProject/src/Grapher.java
+++ VoteProject/src/Grapher.java
@@ -1,4 +1,38 @@
 
 public class Grapher {
-
+	public static void barGraphState(State state) {
+		
+	}
+	
+	public static void barGraphCounty(County county) {
+		
+	}
+	
+	public static void barGraphDistrict(District district) {
+		
+	}
+	
+	public static void pieChartState(State state) {
+		
+	}
+	
+	public static void pieChartCounty(County county) {
+		
+	}
+	
+	public static void pieChartDistrict(District district) {
+		
+	}
+	
+	public static void textState(State state) {
+		
+	}
+	
+	public static void textCounty(County county) {
+		
+	}
+	
+	public static void textDistrict(District district) {
+		
+	}
 }
