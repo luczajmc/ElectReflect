7e900fb1db9a9f7035a80de6a5bcb9be005df92b
Steven Bower <Steven Bower>
2016-11-10 18:01:34 -0500

    First real commit; got the actionListener working for pie charts

--- VoteProject/src/Gui.java
+++ VoteProject/src/Gui.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
 public class Gui extends JPanel{
 	
 	private JFrame window = new JFrame("ElectReflect");
 	private JButton start = new JButton();
+	private JComboBox graphSelect = new JComboBox();
 	
 	public Gui(){
 		super();
@@ -18,14 +21,30 @@ public class Gui extends JPanel{
 		
 		start.setText("Start");
 		start.setLayout(null);
-		start.setLocation(0,100);
+		start.setLocation(10,400);
 		start.setSize(100,50);
 		add(start);
 		start.setMnemonic(KeyEvent.VK_ENTER);
-	}
-	
-	public void start(){
 		
+		start.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(graphSelect.getSelectedItem().equals("Pie Chart")){
+					Grapher.pieChart(new State());
+				}
+			}
+			
+		});
+		
+		graphSelect.setLayout(null);
+		graphSelect.setLocation(10, 300);
+		graphSelect.setSize(100, 25);
+		add(graphSelect);
+		graphSelect.addItem("Choose Display");
+		graphSelect.addItem("Bar Graph");
+		graphSelect.addItem("Pie Chart");
+		graphSelect.addItem("Text Summary");
 	}
 	
 	public static void main(String[] args){
